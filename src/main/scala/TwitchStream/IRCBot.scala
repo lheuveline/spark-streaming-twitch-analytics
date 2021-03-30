02286e59c30713430849e010b0092954f24aa78e
neadex <l.heuveline@louisheuveline.com>
2021-03-30 13:43:59 +0200

    clean IRCBot

---
+++ src/main/scala/TwitchStream/IRCBot.scala
@@ -0,0 +1,67 @@
+package TwitchStream
+
+import java.io._
+import java.net._
+
+import org.apache.spark.internal.Logging
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.receiver.Receiver
+
+class IRCBot(val channel: String) extends Receiver[String](StorageLevel.MEMORY_AND_DISK_2) with Logging {
+
+  // The server to connect to and our details.
+  val server = "irc.chat.twitch.tv"
+  val nick = ""
+  val pass = ""
+  var line: String = null
+
+  @throws[Exception]
+  def read(): Unit = {
+
+    // Connect directly to the IRC server.
+    val socket = new Socket(server, 6667)
+    val writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream,"Cp1252"))
+    val reader = new BufferedReader(new InputStreamReader(socket.getInputStream))
+
+    // Log on to the server.
+    System.out.println("Trying to authenticate")
+    writer.write("PASS " + pass + "\r\n")
+    writer.write("NICK " + nick + "\r\n")
+    writer.flush()
+
+    try {
+      Stream.continually(reader.readLine()).takeWhile(_ != ":tmi.twitch.tv 376 dataflemme :>") foreach { line =>
+        println(line + "\r\n")
+      }
+    } catch {
+      case e: java.net.SocketException => read()// If SocketException, retry to connect
+    }
+
+    // Join the channel.
+    println("Trying to join " + channel)
+    writer.write("JOIN " + channel + "\r\n")
+    writer.flush()
+
+    Stream.continually(reader.readLine()).takeWhile(_ != null) foreach { line =>
+      if (line.toLowerCase.startsWith("PING ")) {
+        writer.write("PONG " + line.substring(5) + "\r\n")
+        writer.write("PRIVMSG " + channel + " :I got pinged!\r\n")
+        writer.flush()
+      }
+      else if (line.contains("PRIVMSG")) {
+        store(line)
+      }
+    }
+  }
+
+  def onStart(): Unit = {
+    val botThread = new Thread {
+      override def run() { read() }
+    }
+    botThread.start()
+  }
+
+  def onStop(): Unit = {
+    // Thread should stop by itself
+  }
+}
